cc4a404d0a9c40aa24b07e57ca82586acc04df5b
Yawo Siatitse <asiatits@u.rochester.edu>
2019-03-23 21:32:23 -0400

    added used routes

--- routes/index.js
+++ routes/index.js
@@ -19,10 +19,57 @@ router.post("/register", function(req, res){
         }
         passport.authenticate("local")(req, res, function(){
            req.flash("success", "Welcome to YelpCamp " + user.username);
-           res.redirect("/jobs"); 
+           res.redirect("/"); 
         });
     });
 });
+
+
+// SHOW - shows more info about one job
+router.get("/:id", function(req, res){
+    //find the job with provided ID
+    User.findById(req.params.id, function(err, foundUser){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(foundUser)
+            //render show template with that job
+            res.json({"user" : foundUser});
+        }
+    });
+});
+
+// EDIT  ROUTE
+router.get("/:id/edit", function(req, res){
+    User.findById(req.params.id, function(err, foundUser){
+        res.render("/edit", {job: foundUser});
+    });
+});
+
+// UPDATE  ROUTE
+router.put("/:id", function(req, res){
+    // find and update the correct job
+    User.findByIdAndUpdate(req.params.id, req.body.job, function(err, updatedUser){
+       if(err){
+           res.redirect("/");
+       } else {
+           //redirect somewhere(show page)
+           res.redirect("/" + req.params.id);
+       }
+    });
+});
+
+// DESTROY  ROUTE
+router.delete("/:id", function(req, res){
+   User.findByIdAndRemove(req.params.id, function(err){
+      if(err){
+          res.status(400).send("Error");
+      } else {
+          res.status(200).send("Success");
+      }
+   });
+});
+
 
 //show login form
 router.get("/login", function(req, res){
@@ -32,7 +79,7 @@ router.get("/login", function(req, res){
 //handling login logic
 router.post("/login", passport.authenticate("local", 
     {
-        successRedirect: "/jobs",
+        successRedirect: "/",
         failureRedirect: "/login"
     }), function(req, res){
 });
@@ -41,7 +88,7 @@ router.post("/login", passport.authenticate("local",
 router.get("/logout", function(req, res){
    req.logout();
    req.flash("success", "Logged you out!");
-   res.redirect("/jobs");
+   res.redirect("/");
 });
 
 
